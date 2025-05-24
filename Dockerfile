c75a4578de7cc91ee6f229911547a20215e52e2b
C2RLO <karol-preiskorn@users.noreply.github.com>
2025-05-24 21:21:32 +0200

    chore: update dependencies and version in package.json
    feat: enhance error handling in models router and add detailed documentation for routes
    - Improved error handling for database connections and operations in models.ts
    - Added JSDoc comments for better understanding of route functionalities and risks
    - Ensured proper closure of database connections in all routes
    - Added validation for DELETE operations to prevent accidental data loss in production

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@
 # The final image is based on the slim version of Node.js to reduce the size and attack surface.
 
 # Stage 1: Build
-FROM node:lts as builder
-RUN apt update && \
-  apt install -y g++ make && \
-  apt clean && \
-  rm -rf /var/lib/apt/lists/*
+FROM node:lts-slim AS builder
+# RUN apt update && \
+#   apt install -y g++ make && \
+#   rm -rf /var/lib/apt/lists/* && \
+#   apt clean
 WORKDIR /usr/src/3d-inventory-api
 COPY package.json package-lock.json ./
 RUN npm install
@@ -18,17 +18,16 @@ COPY . .
 RUN npm run build
 
 # Stage 2: Runtime
-FROM node:lts-slim
-RUN apt update && \
-  apt upgrade -y && \
-  apt install curl iproute2 vim -y && \
-  apt clean && \
-  rm -rf /var/lib/apt/lists/*
+FROM node:lts-slim AS runtime
+# RUN apt update && \
+#   apt install curl iproute2 vim -y && \
+#   rm -rf /var/lib/apt/lists/* && \
+#   apt clean
 WORKDIR /usr/src/3d-inventory-api
 COPY --from=builder /usr/src/3d-inventory-api/dist ./dist
 COPY --from=builder /usr/src/3d-inventory-api/node_modules ./node_modules
 COPY .env api.yaml ./
-# Ensure the cert directory exists in the build context before copying
+RUN mkdir -p cert
 COPY cert ./cert
-EXPOSE 3001
 CMD ["node", "dist/src/index.js"]
+EXPOSE 3001
--- src/routers/models.ts
+++ src/routers/models.ts
@@ -4,13 +4,11 @@
  * @version 2023-12-29  C2RLO - Initial
  **/
 
-import express, { RequestHandler } from 'express';
-import {
-    Collection, Db, DeleteResult, InsertOneResult, ObjectId, OptionalId, UpdateFilter
-} from 'mongodb';
+import express, { RequestHandler } from 'express'
+import { Collection, Db, DeleteResult, InsertOneResult, ObjectId, OptionalId, UpdateFilter } from 'mongodb'
 
-import { connectionClose, connectToCluster, connectToDb } from '../utils/db.js';
-import log from '../utils/logger.js';
+import { connectionClose, connectToCluster, connectToDb } from '../utils/db.js'
+import log from '../utils/logger.js'
 
 const logger = log('models')
 
@@ -36,24 +34,47 @@ const collectionName = 'models'
 const router: express.Router = express.Router()
 
 router.get('/', (async (req, res) => {
-  const client = await connectToCluster()
+  let client
+  try {
+    client = await connectToCluster()
+  } catch (error) {
+    logger.error('Error connecting to the database:', error)
+    res.status(500).send('Internal Server Error')
+    return
+  }
   const db: Db = connectToDb(client)
   const collection: Collection = db.collection(collectionName)
   const results: object[] = await collection.find({}).limit(100).toArray()
   if (!results) {
     logger.warn('GET /models - not found')
     res.status(404).send('Not found')
-  }
-  else {
+  } else {
     logger.info('GET /models - oki return ' + results.length + ' models')
     res.status(200).json(results)
   }
-  await connectionClose(client)
+  if (client) {
+    try {
+      await connectionClose(client)
+    } catch (error) {
+      logger.error('Error closing the database connection:', error)
+    }
+  }
 }) as RequestHandler)
 
+/**
+ * @description Retrieves a model from the database by its ID.
+ * @param {string} id - The ID of the model to retrieve.
+ * @returns {object} - The retrieved model object.
+ * @throws {Error} - If the ID is invalid or if the model is not found.
+ *
+ * @risk This route will expose sensitive information about a specific model in the database.
+ * Ensure that this operation is only performed in a controlled environment, such as during development or testing.
+ * In production, this route should be used with caution to prevent unauthorized access to sensitive data.
+ */
 router.get('/:id', (async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.sendStatus(404)
+    return
   }
   const client = await connectToCluster()
   const db: Db = connectToDb(client)
@@ -65,6 +86,17 @@ router.get('/:id', (async (req, res) => {
   await connectionClose(client)
 }) as RequestHandler)
 
+/**
+ * @description Updates a model in the database by its ID.
+ * @param {string} id - The ID of the model to update.
+ * @param {Model} model - The updated model data.
+ * @returns {object} - The result of the update operation.
+ * @throws {Error} - If the ID is invalid or if the update fails.
+ *
+ * @risk This route will modify an existing model in the database.
+ * Ensure that this operation is only performed in a controlled environment, such as during development or testing.
+ * In production, this route should be used with caution to prevent accidental data loss.
+ */
 router.put('/:id', (async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.status(404).send('Not correct id')
@@ -111,6 +143,7 @@ router.post('/', (async (req, res) => {
 router.patch('/dimension/:id', (async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.sendStatus(404)
+    return
   }
   const query = { _id: new ObjectId(req.params.id) }
   const updates: UpdateFilter<Document>[] = [{ $push: { dimension: req.body as Dimension } }]
@@ -122,6 +155,17 @@ router.patch('/dimension/:id', (async (req, res) => {
   await connectionClose(client)
 }) as RequestHandler)
 
+/**
+ * @description Updates the texture of a model in the database.
+ * @param {string} id - The ID of the model to update.
+ * @param {Texture} texture - The new texture data to set.
+ * @returns {object} - The result of the update operation.
+ * @throws {Error} - If the ID is invalid or if the update fails.
+ *
+ * @risk This route will modify the texture of a model in the database.
+ * Ensure that this operation is only performed in a controlled environment, such as during development or testing.
+ * In production, this route should be used with caution to prevent accidental data loss.
+ */
 router.patch('/texture/:id', (async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.sendStatus(404)
@@ -136,27 +180,84 @@ router.patch('/texture/:id', (async (req, res) => {
   await connectionClose(client)
 }) as RequestHandler)
 
+/**
+ * @description Deletes a document from the 'models' collection by its ID.
+ * @param {string} id - The ID of the document to delete.
+ * @returns {object} - The result of the deletion operation.
+ * @throws {Error} - If the ID is invalid or if the deletion fails.
+ *
+ * @risk This route will permanently delete a document from the 'models' collection.
+ * Ensure that this operation is only performed in a controlled environment, such as during development or testing.
+ * In production, this route should be used with caution to prevent accidental data loss.
+ */
 router.delete('/:id', (async (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
+    logger.warn(`DELETE /models/:id - Invalid ID: ${req.params.id}`)
     res.sendStatus(404)
+    return
   }
-  const query = { _id: new ObjectId(req.params.id) }
   const client = await connectToCluster()
   const db: Db = connectToDb(client)
   const collection: Collection = db.collection(collectionName)
-  const result = await collection.deleteOne(query)
-  res.status(200).json(result)
-  await connectionClose(client)
+  const query = { _id: new ObjectId(req.params.id) }
+  let result: DeleteResult
+  try {
+    result = await collection.deleteOne(query)
+    if (!result) {
+      res.status(500).send('Failed to delete the document')
+    } else if (result.deletedCount === 0) {
+      res.status(404).send('No document found to delete')
+    } else {
+      logger.info(`DELETE /models/:id - oki deleted model with ID: ${req.params.id}`)
+      res.status(200).json(result)
+    }
+  } finally {
+    if (client) {
+      try {
+        await connectionClose(client)
+      } catch (error) {
+        logger.error('Error closing the database connection:', error)
+      }
+    }
+  }
 }) as RequestHandler)
 
-router.delete('/', (async (_req, res) => {
+/**
+ * @description Deletes all documents in the 'models' collection.
+ * @warning This operation is irreversible and will remove all data from the collection.
+ * Use with caution, especially in production environments.
+ *
+ * @risk This route will permanently delete all documents in the 'models' collection.
+ * Ensure that this operation is only performed in a controlled environment, such as during development or testing.
+ * In production, this route is forbidden to prevent accidental data loss.
+ * Always double-check the confirmation query parameter before proceeding.
+ */
+router.delete('/', (async (req, res) => {
+  if (process.env.NODE_ENV === 'production') {
+    logger.warn('DELETE /models - Attempt to delete all documents in production environment')
+    res.status(403).send('Forbidden in production environment')
+    return
+  }
+
+  if (req.query.confirm !== 'true') {
+    logger.warn('DELETE /models - Missing confirmation query parameter')
+    res.status(400).send('Confirmation query parameter "confirm=true" is required')
+    return
+  }
+
   const query = {}
   const client = await connectToCluster()
   const db = connectToDb(client)
-  const collection: Collection = db.collection(collectionName) // Remove the explicit type annotation for 'collection'
-  const result: DeleteResult = await collection.deleteMany(query)
-  res.status(200).json(result)
-  await connectionClose(client)
+  const collection = db.collection(collectionName) // TypeScript infers the type automatically
+  try {
+    const result: DeleteResult = await collection.deleteMany(query)
+    res.status(200).json(result)
+  } catch (error) {
+    logger.error('DELETE /models - Error deleting documents:', error)
+    res.status(500).send('Internal Server Error')
+  } finally {
+    await connectionClose(client)
+  }
 }) as RequestHandler)
 
 export default router
